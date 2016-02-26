f78968baeb1b426483beb971210cccd5b9888ce4
John Calabrese <xchapter7x@gmail.com>
2016-02-25 21:02:24 -0500

    getting main ready for some service bindings

--- main.go
+++ main.go
@@ -12,22 +12,48 @@ import (
 )
 
 func main() {
-	var (
-		username string
-		password string
-		loginurl string
-		ccurl    string
-	)
-	chaos := chaospeddler.NewServiceBroker(chaospeddler.NewMaestro(username, password, loginurl, ccurl))
+	dbInfo := ExtractDBInfo()
+	basicAuthInfo := ExtractBasicAuthInfo()
+	cloudControllerInfo := ExtractCloudControllerInfo()
+	chaos := chaospeddler.NewServiceBroker(chaospeddler.NewMaestro(cloudControllerInfo.Username, cloudControllerInfo.Password, cloudControllerInfo.LoginURL, cloudControllerInfo.CCURL))
 	chaos.Start()
 	logger := lager.NewLogger("chaos-peddler-servicebroker")
 	credentials := brokerapi.BrokerCredentials{
-		Username: "username",
-		Password: "password",
+		Username: basicAuthInfo.Username,
+		Password: basicAuthInfo.Password,
 	}
 	var dbm *mgodb.Dbm
-	_ = dbm.Init("connectUrl", "dbName", 10)
+	_ = dbm.Init(dbInfo.ConnectionURL, dbInfo.DBName, 10)
 	brokerAPI := brokerapi.New(chaos, logger, credentials)
 	http.Handle("/", brokerAPI)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func ExtractDBInfo() (dbInfo DBInfo) {
+	return
+}
+
+func ExtractBasicAuthInfo() (basicAuthInfo BasicAuthInfo) {
+	return
+}
+
+func ExtractCloudControllerInfo() (cloudControllerInfo CloudControllerInfo) {
+	return
+}
+
+type CloudControllerInfo struct {
+	Username string
+	Password string
+	LoginURL string
+	CCURL    string
+}
+
+type BasicAuthInfo struct {
+	Username string
+	Password string
+}
+
+type DBInfo struct {
+	ConnectionURL string
+	DBName        string
 }
