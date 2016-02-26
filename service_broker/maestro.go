820c932d47a2786800062b22a57051249784eab4
John Calabrese <xchapter7x@gmail.com>
2016-02-26 00:12:10 -0500

    remove unused methods. maybe a future feature but who cares right now

--- service_broker/maestro.go
+++ service_broker/maestro.go
@@ -10,21 +10,11 @@ import (
 
 //Start - starts the maestro orchestrating its chaotic ways.
 func (s *Maestro) Start() {
-	s.cleanup()
 	c := cron.New()
 	c.AddFunc("@every 1m", s.PollCrazyPlans)
 	c.AddFunc("@every 10m", s.PollAnnoyingPlans)
 	c.AddFunc("@every 30m", s.PollMickeyMousePlans)
-	c.AddFunc("@every 15m", s.ExpireCrazyRunlist)
-	c.AddFunc("@every 45m", s.ExpireAnnoyingRunlist)
-	c.AddFunc("@every 2h", s.ExpireMickeyMouseRunlist)
 	c.Start()
-}
-
-func (s *Maestro) cleanup() {
-	s.ExpireCrazyRunlist()
-	s.ExpireAnnoyingRunlist()
-	s.ExpireMickeyMouseRunlist()
 }
 
 //PollCrazyPlans -
@@ -40,21 +30,6 @@ func (s *Maestro) PollAnnoyingPlans() {
 //PollMickeyMousePlans -
 func (s *Maestro) PollMickeyMousePlans() {
 	s.poll(MickeyMouseChaosPlanID, ChaosPeddlerServiceID, KillPercentMickeyMouse)
-}
-
-//ExpireCrazyRunlist -
-func (s *Maestro) ExpireCrazyRunlist() {
-	lo.G.Info("expire crazy chaos log")
-}
-
-//ExpireAnnoyingRunlist -
-func (s *Maestro) ExpireAnnoyingRunlist() {
-	lo.G.Info("expire annoying chaos log")
-}
-
-//ExpireMickeyMouseRunlist -
-func (s *Maestro) ExpireMickeyMouseRunlist() {
-	lo.G.Info("expire mickeymouse chaos log")
 }
 
 func (s *Maestro) poll(planid, serviceid string, percent int) {
